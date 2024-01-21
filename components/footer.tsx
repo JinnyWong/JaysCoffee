f5d3dccb5ba84504f7a10c4febdfa8166a676d69
Jinny <jinnywong2002@gmail.com>
2024-01-22 00:12:58 +0800

    fix: footer escaped entities

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="bg-white border-t">
       <div className="mx-auto py-10">
         <p className="text-center text-xs text-black">
-          &copy; 2024 Jay's Coffee. All rights reserved.
+          &copy; 2024 Jay&apos; Coffee. All rights reserved.
         </p>
         <p className="text-center text-xs text-black">
           Address: Room 07, 2nd Floor, Hopson One Mall (No. 2228 Huangxing Road), Yangpu District, Shanghai. 
