2987e27153c8304296f66aa4d065a2b61dbf0254
Jinny <jinnywong2002@gmail.com>
2024-01-23 20:28:33 +0800

    feat: cache control

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 's-max-age=1, stale-while-revalidate=59',
+          },
+        ],
+      },
+    ];
+  },
   images: {
     domains: [
       "tailwindui.com",
